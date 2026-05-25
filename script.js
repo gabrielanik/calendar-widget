a5ca7372698d64045574d9360e1af81ec8f38e06
gabrielanik <gabriela_nikolova@yahoo.com>
2026-05-25 20:27:06 +0300

    CSS change

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function buildCalendar() {
         
             const app = document.querySelector('.app');
             if (app) {
-                app.style.background = isDark ? '#2f2f2f' : '#E2E2E2';
+                app.style.background = isDark ? '#2f2f2f' : '#E9E9E9';
             }
 
             const header = document.querySelector('.header');
